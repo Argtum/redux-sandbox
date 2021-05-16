9e0b1f7c18aec8a299e9e89ae8b31bd8af4666f8
Argtum <silwar40k@gmail.com>
2021-05-16 16:11:56 +0300

    сделал редбюсер чистой функцией

--- src/index.js
+++ src/index.js
@@ -4,15 +4,44 @@ const reducer = (state = 0, action) => {
   switch (action.type) {
     case `INC`:
       return state + 1;
+    case `DEC`:
+      return state - 1;
+    case `RND`:
+      return state + action.payload;
     default:
       return state;
   }
 };
 
 const store = createStore(reducer);
-store.subscribe(() => {
-  console.log(store.getState());
-})
 
-store.dispatch({type: `INC`})
-store.dispatch({type: `INC`})
+document
+  .querySelector(`#inc`)
+  .addEventListener(`click`, () => {
+    store.dispatch({type: `INC`});
+  });
+
+document
+  .querySelector(`#dec`)
+  .addEventListener(`click`, () => {
+    store.dispatch({type: `DEC`});
+  });
+
+document
+  .querySelector(`#rnd`)
+  .addEventListener(`click`, () => {
+    const payload =  Math.floor(Math.random() * 10);
+
+    store.dispatch({
+      type: `RND`,
+      payload
+    });
+  });
+
+const update = () => {
+  document
+    .querySelector(`#counter`)
+    .innerHTML = store.getState();
+}
+
+store.subscribe(update);
