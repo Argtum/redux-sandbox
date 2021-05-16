25436e94a767f15110d51b2f2bcdfff4f3421456
Argtum <silwar40k@gmail.com>
2021-05-16 16:53:44 +0300

    рефакторинг с использование bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-import {createStore} from "redux";
+import {createStore, bindActionCreators} from "redux";
 import reducer from "./reducer";
-import {inc, dec, rnd} from "./actions";
+import  * as actions from "./actions";
 
 const store = createStore(reducer);
+const {dispatch} = store;
+
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
 document
   .querySelector(`#inc`)
-  .addEventListener(`click`, () => {
-    store.dispatch(inc());
-  });
+  .addEventListener(`click`, inc);
 
 document
   .querySelector(`#dec`)
-  .addEventListener(`click`, () => {
-    store.dispatch(dec());
-  });
+  .addEventListener(`click`, dec);
 
 document
   .querySelector(`#rnd`)
   .addEventListener(`click`, () => {
     const payload =  Math.floor(Math.random() * 10);
 
-    store.dispatch(rnd(payload));
+    rnd(payload);
   });
 
 const update = () => {
